c4fa32c1c65e620f1744eaf7c15b70600b2b6599
JustinNg26 <justinng878@gmail.com>
2022-12-01 21:47:03 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+
+  
+
+
+  return (
+    <div className="App">
+      <h1>project 3</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
+// pseudo code
+// MVP
+
+// user selects a body part from the drop down menu.
+
+// what is returned is a list of example exercises they could do for each muscle group including an image.
