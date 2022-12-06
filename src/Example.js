6c1035bb5686aa258350dc128ed874d4013f3d7a
JustinNg26 <justinng878@gmail.com>
2022-12-06 21:46:32 +0000

    merge to development

--- src/Example.js
+++ src/Example.js
@@ -3,19 +3,19 @@
 const Example = ({exercise}) => {
   return (
     <div>
-         <ul>
-            <li>
-                <img 
-                  src={exercise.gifUrl} 
-                  alt={exercise.name} 
-                />
-            </li>
-        </ul>
-        <h2>Body part: {exercise.bodyPart}</h2>
-        <h2>Target Muscle: {exercise.target}</h2>
-        <p>Name of Exercise: {exercise.name}</p>
-        
-
+        <section>
+            <ul>
+                <li>
+                    <img 
+                    src={exercise.gifUrl} 
+                    alt={exercise.name} 
+                    />
+                </li>
+            </ul>
+            <h2>Body part: {exercise.bodyPart}</h2>
+            <h2>Target Muscle: {exercise.target}</h2>
+            <p>Name of Exercise: {exercise.name}</p>
+        </section>
     </div>
   )
 }
