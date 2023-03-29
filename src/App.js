e5cff225cd11f621b7d9e3d04f6f9c0f59b37e8d
justin <justin.ng26@hotmail.com>
2023-03-29 17:55:37 +0100

    installed packages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import './App.css';
-import { useState } from 'react';
+// import { useState } from 'react';
 import {Route, Routes} from 'react-router-dom';
 import {Box} from '@mui/material';
 
-import Search from './Search.js';
-import Results from './Results.js';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+// import Search from './Search.js';
+// import Results from './Results.js';
 
 function App() {
 
   // create a piece of state that will reference the user input.
-  const [exercises, setExercises] = useState([]);
+  // const [exercises, setExercises] = useState([]);
 
   return (
-    <Box width="400px">
-      {/* <Navbar></Navbar> */}
+    <Box width="400px" sx={{width: {xl: '1488px' }}} m="auto">
+      <Navbar />
       <Routes>
         <Route path="/" element= {<Home />} />
         <Route path="/exercises/:id" element= {<ExerciseDetail />} />
 
       </Routes>
+      <Footer />
     </Box>
 
 
