9bfa383cfa04f035ee3b9e85a9c47ab1c1e4a84e
justin <justin.ng26@hotmail.com>
2023-03-30 13:44:55 +0100

    finished banner

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -19,6 +19,8 @@ const HeroBanner = () => {
 {/* Sweat, smile, repeat */}
         <Typography
             fontWeight={700}
+            mb="23px"
+            mt="30px"
             sx={{
                 fontSize: { lg: '44px', xs: '40px'}
             }}>
@@ -29,17 +31,37 @@ const HeroBanner = () => {
         <Typography
             fontSize="22px"
             LineHeight="35px"
-            mb= {3}>
+            mb= {4}>
         Check out the most effective exercises
+        </Typography>
+
+{/* Exercise */}
+        <Typography
+            fontWeight={600}
+            fontSize="200px"
+            color="#ff2625"
+            sx={{
+                opacity: 0.1,
+                display: { lg: 'block', xs: 'none'}}}>
+        Exercise 
         </Typography>
 
 {/* Button */}
         <Button
             variant="contained"
             color="error"
-            href="#exercises">
+            href="#exercises"
+            sx={{
+                backgroundColor: "#ff2625",
+                padding: '10px'}}>
         Explore Exercises
         </Button>
+
+        <img
+          src= {HeroBannerImage}
+          alt= "banner"
+          className="hero-banner-img"
+           />
 
    </Box>
 
