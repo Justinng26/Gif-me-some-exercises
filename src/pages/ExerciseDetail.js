a24f1b5188ef06fc9633c42e7b8a6a261abd6c39
justin <justin.ng26@hotmail.com>
2023-04-18 14:42:30 +0100

    finished home page, now onto exercise detail page

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,6 +1,36 @@
+import {useState, useEffect} from 'react';
+import {useParams} from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { exerciseOptions, fetchData } from '../utils/fetchRequest';
+import Detail from '../components/Detail'
+import ExerciseVideos from '../components/ExerciseVideos'
+import SimilarExercises from '../components/SimilarExercises'
+
+
+
+
 const ExerciseDetail = () => {
+
+  const [exerciseDetail, setExerciseDetail] = useState({});
+  const {id} = useParams();
+
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com'
+      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com'
+    }
+
+    fetchExercisesData()
+  },[id]);
+
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+      <Detail />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
   )
 }
 export default ExerciseDetail
