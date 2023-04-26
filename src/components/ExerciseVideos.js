a1185f7196c84df7647c16b7fd690147ce3c4022
justin <justin.ng26@hotmail.com>
2023-04-26 12:47:50 +0100

    finished videos

--- src/components/ExerciseVideos.js
+++ src/components/ExerciseVideos.js
@@ -10,7 +10,7 @@ const ExerciseVideos = ({ exerciseVideos, name }) => {
     <Box sx={{ marginTop: { lg:'200px', xs:'20px'}}} p="20px">
 
       <Typography variant="h3" mb="33px">
-        Watch
+        Watch 
          <span style={{color: '#ff2625',textTransform: 'capitalize'}}>
               {name}  
          </span> exercise videos
@@ -27,21 +27,29 @@ const ExerciseVideos = ({ exerciseVideos, name }) => {
 
         {exerciseVideos?.slice(0,3).map((item, index) => {
           return (
+              <a
+                key={index}
+                className="exercise-video"
+                href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
+                target="_blank"
+                rel="noreferrer">
+              
+              <img src={item.video.thumbnails[0].url}
+                  alt={item.video.title} />
 
-          
-          <a
-            key={index}
-            className="exercise-video"
-            href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
-            target="_blank"
-            rel="noreferrer">
-          
-          <img src={item.video.thumbnails[0].url}
-               alt={item.video.title} />
-
-          </a>
-          );
-        })}
+              <Box>
+{/* Video title */}
+                <Typography variant="h5" color="#000">
+                  {item.video.title}
+                </Typography>
+{/* Channel title */}
+                <Typography variant="h6" color="#000">
+                  {item.video.channelName}
+                </Typography>
+              </Box>
+              </a>
+              );
+            })}
       </Stack>
     </Box>
   )
